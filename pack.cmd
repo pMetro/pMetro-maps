b7d697e82c3fdcf0183b2ff71923f7d15c7be65b
pMetro <pmetro.git@gmail.com>
2020-02-02 15:43:28 +0300

    update from pMetro.su

--- pack.cmd
+++ pack.cmd
@@ -3,13 +3,14 @@ SETLOCAL EnableDelayedExpansion
 
 set ZIP7=7z.exe
 set ZIP_DIR=download
+set PMZ_DIR=pmz
 set MAPS_DIR=maps
 
 
 del /qf %ZIP_DIR%\* > nul
 
 FOR /D %%G in ("%MAPS_DIR%\*") DO (
-  %ZIP7% a -tzip -bb0 %ZIP_DIR%\%%~nxG.pmz .\%MAPS_DIR%\%%~nxG\*  > nul
-  %ZIP7% a -tzip -bb0 %ZIP_DIR%\%%~nxG     .\%ZIP_DIR%\%%~nxG.pmz > nul
-  del %ZIP_DIR%\%%~nxG.pmz
+rem  %ZIP7% a -tzip -bb0 %PMZ_DIR%\%%~nxG.pmz .\%MAPS_DIR%\%%~nxG\*  > nul
+  %ZIP7% a -tzip -bb0 %ZIP_DIR%\%%~nxG     .\%PMZ_DIR%\%%~nxG.pmz > nul
+rem  del %PMZ_DIR%\%%~nxG.pmz
  )
